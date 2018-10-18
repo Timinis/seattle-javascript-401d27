8d45e6a8e4520078dd4ea1a10566d711dce189c3
Timinis <litimc3@gmail.com>
2018-10-17 20:59:09 -0700

    fixed the error on lab 1 check the corresponding file to see change

--- 01-node-ecosystem/_test_/lib/greet.test.js
+++ 01-node-ecosystem/_test_/lib/greet.test.js
@@ -5,9 +5,7 @@
 const helloModule = require('../../lib/greet.js');
 
 describe('Testing the helloModule', () => {
-
   it(`should say "hello" + " string" when function is invoked`, () => {
-
     expect(helloModule('world')).toBe('Hello world');
   });
   it(`should say null when function is invoked with failing parameters`, () => {
@@ -17,3 +15,5 @@ describe('Testing the helloModule', () => {
     expect(helloModule(12)).toBe(null);
   });
 });
+
+//added some hotfixes
