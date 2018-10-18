aefc3fae8726237a3336e703ac1a705ae1a51b17
Timinis <litimc3@gmail.com>
2018-10-18 15:12:52 -0700

    finished structuring need work on transform function

--- 04-binary-data/lab-04/index.js
+++ 04-binary-data/lab-04/index.js
@@ -2,88 +2,48 @@
 
 const fs = require('fs');
 
-/**
- * Bitmap -- receives a file name, used in the transformer to note the new buffer
- * @param filePath
- * @constructor
- */
-function Bitmap(filePath) {
-  this.file = filePath;
+class Bitmap {
+  constructor(filePath) {
+    this.file = filePath;
+  }
+
+  //buffer is a data type that is retrieved from using fs.readfile
+  //parse is intended to parse on the type of the file to this object known as Bitmap
+  parse(buffer) {
+    this.type = buffer.toString('utf-8', 0, 2);
+  }
+
+  //this function effectively creates the new file path and a filename for this file
+  //call back is the intended function
+  transform(callBack) {
+    transformDictionary[callBack](this);
+    this.newFile = this.file.replace(/\.bmp/, `.${callBack}.bmp`);
+  }
 }
 
-/**
- * Parser -- accepts a buffer and will parse through it, according to the specification, creating object properties for each segment of the file
- * @param buffer
- */
-Bitmap.prototype.parse = function(buffer) {
-  this.type = buffer.toString('utf-8', 0, 2);
-  //... and so on
+//an object containing different methods for transformation
+const transformDictionary = {
+  //to do list- make a greyscale transformation function in the future
+  // greyscale: transformGreyScale;
 };
 
-/**
- * Transform a bitmap using some set of rules. The operation points to some function, which will operate on a bitmap instance
- * @param operation
- */
-Bitmap.prototype.transform = function(operation) {
-  // This is really assumptive and unsafe
-  transforms[operation](this);
-  this.newFile = this.file.replace(/\.bmp/, `.${operation}.bmp`);
-};
-
-/**
- * Sample Transformer (greyscale)
- * Would be called by Bitmap.transform('greyscale')
- * Pro Tip: Use "pass by reference" to alter the bitmap's buffer in place so you don't have to pass it around ...
- * @param bmp
- */
-const transformGreyscale = (bmp) => {
-
-  console.log('Transforming bitmap into greyscale', bmp);
-
-  //TODO: Figure out a way to validate that the bmp instance is actually valid before trying to transform it
-
-  //TODO: alter bmp to make the image greyscale ...
-
-};
-
-/**
- * A dictionary of transformations
- * Each property represents a transformation that someone could enter on the command line and then a function that would be called on the bitmap to do this job
- */
-const transforms = {
-  greyscale: transformGreyscale
-};
-
-// ------------------ GET TO WORK ------------------- //
-
-function transformWithCallbacks() {
-
+const readFileAndTransform = () => {
   fs.readFile(file, (err, buffer) => {
-
     if (err) {
       throw err;
     }
+    baldy.parse(buffer);
 
-    bitmap.parse(buffer);
+    //callBack still needs to be created refer back to transformDictionary
+    baldy.transform(callBack);
 
-    bitmap.transform(operation);
-
-    // Note that this has to be nested!
-    // Also, it uses the bitmap's instance properties for the name and thew new buffer
-    fs.writeFile(bitmap.newFile, bitmap.buffer, (err, out) => {
-      if (err) {
-        throw err;
-      }
-      console.log(`Bitmap Transformed: ${bitmap.newFile}`);
+    fs.writeFile(baldy.newFile, baldy.buffer, (err, out) => {
+      console.log(`BitMap tranformed: ${baldy.newFile}`);
     });
-
   });
-}
+};
 
-// TODO: Explain how this works (in your README)
-const [file, operation] = process.argv.slice(2);
+const [file, callBack] = process.argv.slice(2);
 
-let bitmap = new Bitmap(file);
-
-transformWithCallbacks();
-
+let baldy = new Bitmap('./assets/baldy.bmp');
+readFileAndTransform();
