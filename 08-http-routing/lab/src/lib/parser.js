f5f0d22e4b52266985899685a3ee621df4369913
Timinis <litimc3@gmail.com>
2018-10-24 18:47:18 -0700

    added today's lab with readme

---
+++ 08-http-routing/lab/src/lib/parser.js
@@ -0,0 +1,34 @@
+'use strict';
+
+//First Party Modules
+const url = require('url');
+const queryString = require('querystring');
+
+//use this to parse the string
+module.exports = req => {
+  return new Promise((resolve, reject) => {
+    if (!(req || req.url)) {
+      reject('Invalid Request Object. Cannot Parse');
+    }
+    req.parsed = url.parse(req.url);
+    req.query = queryString.parse(req.parsed.query);
+    if (!req.method.match(/POST|PUT|PATCH/)) {
+      resolve(req);
+    }
+    let text = '';
+
+    req.on('data', buffer => {
+      text += buffer.toString();
+    });
+
+    req.on('end', () => {
+      try {
+        req.body = JSON.parse(text);
+        resolve(req);
+      } catch (err) {
+        reject(err);
+      }
+    });
+    req.on('err', reject);
+  });
+};
