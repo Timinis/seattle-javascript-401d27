df61389b385ef1fad8110a94ee55c386d5105e1a
Timinis <litimc3@gmail.com>
2018-10-29 18:53:28 -0700

    added working lab

--- 11-express/lab/src/app.js
+++ 11-express/lab/src/app.js
@@ -1,16 +1,17 @@
 import express from 'express';
 // import albumRouter from './routes/albums.js';
+import getAllCats from './api/api.js';
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-
+app.use(express.urlencoded({ extended: true }));
+app.use(getAllCats);
 // app.use(albumRouter);
 
 module.exports = {
   app,
-  start: (port) => {
+  start: port => {
     app.listen(port, () => console.log('Running on', port));
   }
-}
+};
